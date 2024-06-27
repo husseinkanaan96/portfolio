6e6f6dcdb21099d80bd3f6efb28ee3fafee256bd
Hussein Kanaan <husseinkanaan1996@gmail.com>
2024-06-27 13:37:57 +0300

    creating accordions for resume page with downloadable pdf cv button and beautifying other html files

--- angular-portfolio/src/app/resume/resume.component.ts
+++ angular-portfolio/src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,11 +8,24 @@ import { Title } from '@angular/platform-browser';
 })
 export class ResumeComponent {
 
-  constructor(private titleService: Title) {
+  isWorkExpOpen: boolean = false;
+  isEducationOpen: boolean = false;
+  isCertificatesOpen: boolean = false;
+  isSkillsOpen: boolean = false;
+  isLanguagesOpen: boolean = false;
+
+  constructor(private titleService: Title, private renderer: Renderer2) {
 
     this.titleService.setTitle('Hussein M. Kanaan - Resume');
+  }
 
-
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/Hussein Kanaan’s Resume.pdf');
+    link.setAttribute('download', 'Hussein Kanaan’s Resume.pdf');
+    link.click();
+    link.remove();
   }
 
 }
